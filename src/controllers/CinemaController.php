a501b394daabaf2f310e536b91ebbbb739ea2ac9
rplantefeve <romain.plantefeve@gmail.com>
2016-12-09 21:18:22 +0100

    Ajout options d'admin liste des cinémas

--- src/controllers/CinemaController.php
+++ src/controllers/CinemaController.php
@@ -23,13 +23,22 @@ class CinemaController {
      * Route Liste des cinémas
      */
     public function cinemasList() {
+        $isUserAdmin = false;
+
+        session_start();
+        // si l'utilisateur est pas connecté et qu'il est amdinistrateur
+        if (array_key_exists("user", $_SESSION) and $_SESSION['user'] == 'admin@adm.adm') {
+            $isUserAdmin = true;
+        }
         // on récupère la liste des cinémas ainsi que leurs informations
         $cinemas = $this->cinemaDAO->getCinemasList();
 
         // On génère la vue films
         $vue = new View("CinemasList");
         // En passant les variables nécessaires à son bon affichage
-        $vue->generer(['cinemas' => $cinemas]);
+        $vue->generer([
+            'cinemas'     => $cinemas,
+            'isUserAdmin' => $isUserAdmin]);
     }
 
 }
