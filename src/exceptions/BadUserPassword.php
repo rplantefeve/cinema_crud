d80ba869c1a40cb1328ba7292bb7dec32b6d2697
rplantefeve <romain.plantefeve@gmail.com>
2016-12-20 15:28:05 +0100

    Gestion des cas d'erreur de Utilisateur DAO améliorée #2

---
+++ src/exceptions/BadUserPassword.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Semeformation\Mvc\Cinema_crud\exceptions;
+
+/**
+ * Description of BadUserPassword
+ *
+ * @author Seme
+ */
+class BadUserPassword extends \Exception{
+    public function __construct(string $message = "", int $code = 0,
+            \Throwable $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+}
