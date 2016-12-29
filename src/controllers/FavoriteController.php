c8d9cf8ca707e780cca85d4b4e4ebf2f6d6cec5d
rplantefeve <romain.plantefeve@gmail.com>
2016-12-29 17:07:31 +0100

    Ajouter/Modifier une préférence en Twig

--- src/controllers/FavoriteController.php
+++ src/controllers/FavoriteController.php
@@ -2,12 +2,10 @@
 
 namespace Semeformation\Mvc\Cinema_crud\controllers;
 
-use Semeformation\Mvc\Cinema_crud\views\View;
 use Semeformation\Mvc\Cinema_crud\models\Prefere;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Silex\Application;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Description of FavoriteController
@@ -103,6 +101,10 @@ class FavoriteController extends Controller {
             $entries['filmID'] = $filmId;
             $entries['userID'] = $userId;
 
+            if (is_null($entries['userID'])) {
+                $entries['userID'] = $app['session']->get('user')['userId'];
+            }
+
             if ($entries && !is_null($entries['filmID']) && $entries['filmID'] !==
                     '' && !is_null($entries['userID']) && $entries['userID'] !==
                     '') {
@@ -117,24 +119,23 @@ class FavoriteController extends Controller {
                     "commentaire" => $prefere->getCommentaire()];
                 // sinon, c'est une création
             } else {
-                $films       = $app['dao.prefere']->getFilmDAO()->findAllByUserIdNotIn($app['session']->get('user')['userId']);
+                $films       = $app['dao.prefere']->getFilmDAO()->findAllByUserIdNotIn($entries['userID']);
                 // on initialise les autres variables de formulaire à vide
                 $prefere     = new Prefere();
-                $utilisateur = $app['dao.prefere']->getUtilisateurDAO()->find($app['session']->get('user')['userId']);
+                $utilisateur = $app['dao.prefere']->getUtilisateurDAO()->find($entries['userID']);
                 $prefere->setUtilisateur($utilisateur);
             }
         }
 
         $donnees = [
+            'titre'           => 'Ajouter/Modifier une préférence',
             'aFilmIsSelected' => $aFilmIsSelected,
             'prefere'         => $prefere,
-            'userID'          => $userId,
+            'userID'          => $entries['userID'],
             'films'           => $films
         ];
         // On génère la vue Films préférés
-        $vue     = new View("FavoriteMovie");
-        // En passant les variables nécessaires à son bon affichage
-        return $vue->generer($request, $donnees);
+        return $app['twig']->render('favorite.edit.html.twig', $donnees);
     }
 
     /**
