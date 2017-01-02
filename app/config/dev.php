634e13969cc6fe18227085227e72496cbb356c41
rplantefeve <romain.plantefeve@gmail.com>
2017-01-02 14:40:54 +0100

    Ménage

--- app/config/dev.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// inclut la configuration de production
-require __DIR__ . '/prod.php';
-
-// active le mode debug
-$app['debug'] = true;
--- web/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once dirname(__DIR__) . '/src/vendor/autoload.php';
-
-// initialisation de l'application
-require_once dirname(__DIR__) . '/app/app.php';
-
