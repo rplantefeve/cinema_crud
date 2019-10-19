bfddf26c8a3cbc482e28bf511f53d2eafc857d11
R. Plantefève <romain.plantefeve@laposte.net>
2019-10-19 16:07:08 +0200

    Chaîne de connexion mise à jour

--- src/includes/DBFactory.php
+++ src/includes/DBFactory.php
@@ -5,7 +5,8 @@ namespace Semeformation\Mvc\Cinema_crud\includes;
 use PDO;
 use Psr\Log\LoggerInterface;
 
-class DBFactory {
+class DBFactory
+{
 
     // logger
     private $logger;
@@ -14,15 +15,16 @@ class DBFactory {
     // instance de la classe PDO
     private $pdoInstance = null;
     // Champs de connexion à la BDD
-    private $user = "userCinema";
-    private $pass = "pwdCinema";
+    private $user = "user";
+    private $pass = "Us3rUs€r";
     private $dataSourceName = "mysql:host=127.0.0.1;dbname=cinema_crud;charset=utf8";
 
     /*
      * Méthode utile pour récupérer le singleton depuis le programme appelant
      */
 
-    public static function getFactory(LoggerInterface $logger = null) {
+    public static function getFactory(LoggerInterface $logger = null)
+    {
         // si l'instance n'a encore jamais été instanciée
         if (!self::$factory) {
             self::$factory = new DBFactory($logger);
@@ -35,13 +37,16 @@ class DBFactory {
      * Constructeur de la classe qui initialise la connexion
      */
 
-    public function getConnection() {
+    public function getConnection()
+    {
         if (!$this->pdoInstance) {
             // on appelle le constructeur de la classe PDO
-            $this->pdoInstance = new PDO($this->dataSourceName,
+            $this->pdoInstance = new PDO(
+                $this->dataSourceName,
                     $this->user,
                     $this->pass,
-                    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+                    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+            );
             if ($this->logger) {
                 $this->logger->info('Database connection succeeded.');
             }
@@ -49,7 +54,8 @@ class DBFactory {
         return $this->pdoInstance;
     }
 
-    private function __construct(LoggerInterface $logger = null) {
+    private function __construct(LoggerInterface $logger = null)
+    {
         if ($logger) {
             $this->logger = $logger;
         }
@@ -57,14 +63,19 @@ class DBFactory {
 
     // The clone and wakeup methods prevents external instantiation of copies of the Singleton class,
     // thus eliminating the possibility of duplicate objects.
-    public function __clone() {
-        trigger_error('Clone is not allowed.',
-                E_USER_ERROR);
+    public function __clone()
+    {
+        trigger_error(
+            'Clone is not allowed.',
+                E_USER_ERROR
+        );
     }
 
-    public function __wakeup() {
-        trigger_error('Deserializing is not allowed.',
-                E_USER_ERROR);
+    public function __wakeup()
+    {
+        trigger_error(
+            'Deserializing is not allowed.',
+                E_USER_ERROR
+        );
     }
-
 }
