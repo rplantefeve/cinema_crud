9a161b74302ad93ee84cdbf22bfc23ab64b0f8ef
Romain Plantefève <romain.plantefeve@laposte.net>
2024-03-29 20:03:29 +0100

    Updated DB scripts
    (cherry picked from commit 3b9b80c0e2169cb303446a06d5be9f209723cb0a)

--- db/insert_data.sql
+++ db/insert_data.sql
@@ -1,21 +1,19 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.2
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Jeu 03 Septembre 2015 à 17:03
--- Version du serveur :  5.6.20
--- Version de PHP :  5.5.15
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 29, 2024 at 01:41 PM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
+--
+-- Database: `cinema_crud`
+--
 USE `cinema_crud`;
 
 --
@@ -89,6 +87,4 @@ INSERT INTO `utilisateur` (`USERID`, `NOM`, `PRENOM`, `ADRESSECOURRIEL`, `PASSWO
 (19, 'rp', 'rp', 'rp@rp.rp', '$2y$10$5QzyFQLLfzuGNkveZnmqjOLSgxGvQuhxno63tzViYw4E04cgKjk76'),
 (20, 'admin', 'admin', 'admin@adm.adm', '$2y$10$r2maAvjdNftF8bzZMRl8..m6aIxo.jthpa..tgSp1ehDN.YLNFuNe');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;
