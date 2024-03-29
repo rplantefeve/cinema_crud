3b0260b4945eae6ad91a3a4de66477ec52a90840
Romain Plantefève <romain.plantefeve@laposte.net>
2024-03-29 20:03:29 +0100

    Updated DB scripts
    (cherry picked from commit 3b9b80c0e2169cb303446a06d5be9f209723cb0a)
    (cherry picked from commit 9a161b74302ad93ee84cdbf22bfc23ab64b0f8ef)
    (cherry picked from commit 051c6a7a3db889bbe5a23e2b6069f11b82b53ece)
    (cherry picked from commit 9deb0c14cedfed196acfe2787008b57d21ecd2ef)

--- db/create_base.sql
+++ db/create_base.sql
@@ -1,153 +1,95 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.2
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Jeu 03 Septembre 2015 à 17:03
--- Version du serveur :  5.6.20
--- Version de PHP :  5.5.15
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 29, 2024 at 01:41 PM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
--- Base de données :  `cinema_crud`
+-- Database: `cinema_crud`
 --
-CREATE DATABASE IF NOT EXISTS `cinema_crud` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `cinema_crud` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `cinema_crud`;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `cinema`
+-- Table structure for table `cinema`
 --
 
 DROP TABLE IF EXISTS `cinema`;
 CREATE TABLE IF NOT EXISTS `cinema` (
-  `CINEMAID` int(11) NOT NULL,
+  `CINEMAID` int NOT NULL AUTO_INCREMENT,
   `DENOMINATION` varchar(50) NOT NULL,
-  `ADRESSE` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `ADRESSE` varchar(150) NOT NULL,
+  PRIMARY KEY (`CINEMAID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `film`
+-- Table structure for table `film`
 --
 
 DROP TABLE IF EXISTS `film`;
 CREATE TABLE IF NOT EXISTS `film` (
-  `FILMID` int(11) NOT NULL,
+  `FILMID` int NOT NULL AUTO_INCREMENT,
   `TITRE` varchar(100) NOT NULL,
-  `TITREORIGINAL` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `TITREORIGINAL` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`FILMID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `prefere`
+-- Table structure for table `prefere`
 --
 
 DROP TABLE IF EXISTS `prefere`;
 CREATE TABLE IF NOT EXISTS `prefere` (
-  `USERID` int(11) NOT NULL DEFAULT '0',
-  `FILMID` int(11) NOT NULL DEFAULT '0',
-  `COMMENTAIRE` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `USERID` int NOT NULL DEFAULT '0',
+  `FILMID` int NOT NULL DEFAULT '0',
+  `COMMENTAIRE` text,
+  PRIMARY KEY (`USERID`,`FILMID`),
+  KEY `fk_prefere_film` (`FILMID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `seance`
+-- Table structure for table `seance`
 --
 
 DROP TABLE IF EXISTS `seance`;
 CREATE TABLE IF NOT EXISTS `seance` (
-  `CINEMAID` int(11) NOT NULL DEFAULT '0',
-  `FILMID` int(11) NOT NULL DEFAULT '0',
+  `CINEMAID` int NOT NULL DEFAULT '0',
+  `FILMID` int NOT NULL DEFAULT '0',
   `HEUREDEBUT` datetime NOT NULL,
   `HEUREFIN` datetime NOT NULL,
-  `VERSION` varchar(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `VERSION` varchar(6) NOT NULL,
+  PRIMARY KEY (`CINEMAID`,`FILMID`),
+  KEY `fk_seance_film` (`FILMID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `utilisateur`
+-- Table structure for table `utilisateur`
 --
 
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `USERID` int(11) NOT NULL,
+  `USERID` int NOT NULL AUTO_INCREMENT,
   `NOM` varchar(50) NOT NULL,
   `PRENOM` varchar(30) NOT NULL,
   `ADRESSECOURRIEL` varchar(90) NOT NULL,
-  `PASSWORD` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `cinema`
---
-ALTER TABLE `cinema`
-  ADD PRIMARY KEY (`CINEMAID`);
-
---
--- Index pour la table `film`
---
-ALTER TABLE `film`
-  ADD PRIMARY KEY (`FILMID`);
-
---
--- Index pour la table `prefere`
---
-ALTER TABLE `prefere`
-  ADD PRIMARY KEY (`USERID`,`FILMID`),
-  ADD KEY `fk_prefere_film` (`FILMID`);
-
---
--- Index pour la table `seance`
---
-ALTER TABLE `seance`
-  ADD PRIMARY KEY (`CINEMAID`,`FILMID`),
-  ADD KEY `fk_seance_film` (`FILMID`);
-
---
--- Index pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`USERID`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `cinema`
---
-ALTER TABLE `cinema`
-  MODIFY `CINEMAID` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `film`
---
-ALTER TABLE `film`
-  MODIFY `FILMID` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  MODIFY `USERID` int(11) NOT NULL AUTO_INCREMENT;
-
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  `PASSWORD` varchar(255) NOT NULL,
+  PRIMARY KEY (`USERID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
