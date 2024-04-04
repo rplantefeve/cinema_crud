c0bfc362f26b7579435aee7fc17d3cd12d2777b5
Romain Plantefève <romain.plantefeve@laposte.net>
2024-04-04 17:09:13 +0200

    Removed FILTER_SANITIZE_STRING

--- src/editFavoriteMovie.php
+++ src/editFavoriteMovie.php
@@ -31,8 +31,8 @@ if (filter_input(
             ['backToList' => FILTER_DEFAULT,
         'filmID' => FILTER_SANITIZE_NUMBER_INT,
         'userID' => FILTER_SANITIZE_NUMBER_INT,
-        'comment' => FILTER_SANITIZE_STRING,
-        'modificationInProgress' => FILTER_SANITIZE_STRING]);
+        'comment' => FILTER_DEFAULT,
+        'modificationInProgress' => FILTER_DEFAULT]);
 
     // si l'action demandée est retour en arrière
     if ($sanitizedEntries['backToList'] !== NULL) {
