8fe735af6bd170108979d5b22002047da2ddd68e
Romain Plantefève <romain.plantefeve@laposte.net>
2024-04-09 10:35:03 +0200

    Views edit cinema / movie added

---
+++ src/views/viewEditCinema.php
@@ -0,0 +1,23 @@
+<?php $this->title = "Cinéma - Ajouter ou Modifier"; ?>
+<h1>Ajouter/Modifier un cinéma</h1>
+<form method="POST" name="editCinema" action="index.php?action=editCinema">
+    <label>Dénomination :</label>
+    <input name="denomination" type="text" value="<?= $cinema['DENOMINATION'] ?>" required/>
+    <label>Adresse :</label>
+    <textarea name="adresse" required><?= $cinema['ADRESSE'] ?></textarea>
+    <br/>
+    <input type="hidden" value="<?= $cinema['CINEMAID'] ?>" name="cinemaID"/>
+    <?php
+    // si c'est une modification, c'est une information dont nous avons besoin
+    if (!$isItACreation) {
+        ?>
+        <input type="hidden" name="modificationInProgress" value="true"/>
+        <?php
+    }
+    ?>
+    <input type="submit" name="saveEntry" value="Sauvegarder"/>
+</form>
+<form name="cinemasList" method="GET" action="index.php">
+    <input name="action" type="hidden" value="cinemasList"/>
+    <input type="submit" value="Retour à la liste des cinémas"/>
+</form>
